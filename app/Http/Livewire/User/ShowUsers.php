db27d40c76fee541181d8d46cdc5e7fe4c08d3ea
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-03-16 17:37:09 +0100

    Added pagination

--- app/Http/Livewire/User/ShowUsers.php
+++ app/Http/Livewire/User/ShowUsers.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire\User;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowUsers extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.user.show-users', [
-            'users' => User::all()
+            'users' => User::paginate(10),
         ]);
     }
 
