1e99272c339d3bc66d16416196c02ec57880e528
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-07 12:13:27 +0200

    added dusk selectors
    for: Superdmin can add user

--- resources/views/livewire/user/show-users.blade.php
+++ resources/views/livewire/user/show-users.blade.php
@@ -2,11 +2,11 @@
     <div class="px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
-                <h1 class="text-xl font-semibold text-gray-900">{{ __('Users') }}</h1>
+                <h1 dusk="users" class="text-xl font-semibold text-gray-900">{{ __('Users') }}</h1>
                 <p class="mt-2 text-sm text-gray-700">{{ __('A list of all the users') }}</p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                <a href="/user/add" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                <a dusk="add-user" href="/user/add" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                     {{ __('Add user') }}
                 </a>
             </div>
