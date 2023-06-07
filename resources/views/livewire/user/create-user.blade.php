332c696302f98fba71fedccf54ba0bc8e3d9b877
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-07 12:15:31 +0200

    Update create-user.blade.php

--- resources/views/livewire/user/create-user.blade.php
+++ resources/views/livewire/user/create-user.blade.php
@@ -10,7 +10,7 @@
 
             <div class="mb-6">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900">{{ __('Email') }}</label>
-                <input type="email" wire:model="users.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="{{ __('Enter email') }}..." required="">
+                <input dusk="email" type="email" wire:model="users.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="{{ __('Enter email') }}..." required="">
                 @error('users.email') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
             </div>
 
