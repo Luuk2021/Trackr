927b4af2ac59a941f962b59c2d5ceb3f96bbb52d
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-07 13:15:36 +0200

    Update show-users.blade.php

--- resources/views/livewire/user/show-users.blade.php
+++ resources/views/livewire/user/show-users.blade.php
@@ -51,7 +51,7 @@
                                     </td>
                                     <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                         <a id="edit" href="/user/edit/{{ $user->id }}" class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
-                                        <a href="#" onclick="confirm('Are you sure you want to remove the user') || event.stopImmediatePropagation()" wire:click="delete({{ $user->id }})" class="text-red-600 hover:text-red-900">{{ __('Delete') }}</a>
+                                        <a id="delete" href="#" onclick="confirm('Are you sure you want to remove the user') || event.stopImmediatePropagation()" wire:click="delete({{ $user->id }})" class="text-red-600 hover:text-red-900">{{ __('Delete') }}</a>
                                     </td>
                                 </tr>
                                 @endforeach
