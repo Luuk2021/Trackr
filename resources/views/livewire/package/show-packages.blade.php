6ea940b6b68ec465673a565004f7e8e067fd92ce
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-08 11:43:07 +0200

    Update show-packages.blade.php

--- resources/views/livewire/package/show-packages.blade.php
+++ resources/views/livewire/package/show-packages.blade.php
@@ -23,7 +23,7 @@
         <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-300">
+                    <table id="table" class="min-w-full divide-y divide-gray-300">
                         <thead class="bg-gray-50">
                             <tr>
                                 <th class="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6">
