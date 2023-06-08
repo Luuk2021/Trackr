6dad6b5dcaea6c3a2cdfd3f7f9b8e223ad1d535b
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-08 11:57:55 +0200

    Update show-packages.blade.php

--- resources/views/livewire/package/show-packages.blade.php
+++ resources/views/livewire/package/show-packages.blade.php
@@ -14,7 +14,7 @@
                         <input id="file" type="file" wire:model="file">
                         <button dusk="import" type="submit" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Import') }}</button>
                     </form>
-                    <a href="#" wire:click="generateSelectedPDFs()" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Download Selected') }}</a>
+                    <a id="download-bulk" href="#" wire:click="generateSelectedPDFs()" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Download Selected') }}</a>
                 </div>
             </div>
         </div>
