f6be66d0b65587c258961ee33f541caf9cfc9ea3
Tim <59014073+TimothyGH@users.noreply.github.com>
2023-06-08 10:39:42 +0200

    Update show-packages.blade.php

--- resources/views/livewire/package/show-packages.blade.php
+++ resources/views/livewire/package/show-packages.blade.php
@@ -12,7 +12,7 @@
                     </a>
                     <form wire:submit.prevent="import">
                         <input id="file" type="file" wire:model="file">
-                        <button type="submit" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Import') }}</button>
+                        <button dusk="import" type="submit" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Import') }}</button>
                     </form>
                     <a href="#" wire:click="generateSelectedPDFs()" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">{{ __('Download Selected') }}</a>
                 </div>
